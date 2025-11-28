b8510e948ab246df9271d4256c3ec4090b436249
Eddie <ekwak30@gatech.edu>
2025-11-28 18:11:30 -0500

    changed some state transitions

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,6 @@
 #define REALTIME_DATABASE_URL "cat-automated-smart-home-default-rtdb.firebaseio.com"
 
 // Network credentials (will not be pushed)
-const char* WIFI_SSID = "Eddie";
-const char* WIFI_PASSWORD = "Eddiek1102!";
 
 // const char* WIFI_SSID = "SSID";
 // const char* WIFI_PASSWORD = "password";
